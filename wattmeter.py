6f8a40a16ab55122239b0025a60f89ac1e646cf8
Timothy Kheang <tkheang@lindsey.local>
2021-01-28 13:26:40 -0800

    Added functions

--- wattmeter.py
+++ wattmeter.py
@@ -1,44 +1,66 @@
 import psutil
 import time
+import os
+import asyncio
+import kasa
+from dotenv import load_dotenv
 
+def measureWatts(plug):
+    plug.update()
+    emeter = plug.emeter_realtime()
+    # Power is in watts
+    power = emeter["power"]
+    return power
 
-found = False
-startTime = 0.0
-endTime = 0.0
-runTime = 0.0
+def main():
+    load_dotenv()
+    
+    # Try to connect to Kasa smart plug
+    try:
+        deviceIP = os.getenv("deviceIP")
+        plug = kasa.SmartPlug(deviceIP)
+        if plug.is_on:
+            print("Smart plug found!")
+    except:
+        print("Smart plug not found!")
+        exit()
 
-# Program to monitor
-program = "AcroRd32.exe"
+    # Program to monitor
+    program = "AcroRd32.exe"
+    found = False
 
-def measureWatts():
-    return
-
-start = input("Scan for program?: y/n ")
-if start == "y":
-    print("Scanning...")
-    while found == False:
-            if program in (p.name() for p in psutil.process_iter()):
-                startTime = time.time()
-                found = True
-                print("Program found!")
-            else:
-                print("Still scanning...")
-            time.sleep(1)
-else:
-    exit()
-
-while found == True:
-    # Exit when program has stopped running
-    if program not in (p.name() for p in psutil.process_iter()):
-        endTime = time.time()
-        found = False
-        print("Program has stopped running")
+    if input("Scan for program?: y/n ") == "y":
+        print("Scanning...")
+        while found == False:
+                if program in (p.name() for p in psutil.process_iter()):
+                    startTime = time.time()
+                    found = True
+                    print("Program found!")
+                else:
+                    print("Still scanning...")
+                    time.sleep(1)
     else:
-        print("Program is still running...")
-        measureWatts()
-        time.sleep(1)
+        exit()
 
-runTime = endTime - startTime
+    while found == True:
+        # Exit when program has stopped running
+        if program not in (p.name() for p in psutil.process_iter()):
+            endTime = time.time()
+            found = False
+            print("Program has stopped running")
+        else:
+            print("Program is still running...")
+            currentPower = measureWatts(plug)
+            totalConsumption += currentPower
+            count += 1
+            print("Current power consumption: " + currentPower + " watts")
+            time.sleep(1)
 
-print("Program has run for " + str(runTime) + " seconds")
-print("Power consumed during runtime: ")
+    totalConsumption = totalConsumption / count
+    runTime = endTime - startTime
+
+    print("Program has run for " + str(runTime) + " seconds")
+    print("Average power consumed during runtime: " + totalConsumption + " watts")
+
+if __name__== "__main__":
+  main()
